e757c09a8a3eb23d56b59472ea6aad3020ae70eb
milesfleisher <68367314+milesfleisher@users.noreply.github.com>
2025-09-02 22:40:26 -0400

    Update intussusception_trainer.py

--- Source Code/intussusception_trainer.py
+++ Source Code/intussusception_trainer.py
@@ -1435,8 +1435,8 @@ class ARIanaApp:
 
         self.pressure_frame = ttk.Frame(self.pressure_input_frame)
         ttk.Label(self.pressure_frame, text="Pressure (mmHg):").pack(side=tk.LEFT)
- 
         self.pressure_display_label = ttk.Label(self.pressure_frame, text="0", width=4)
+        #this pressure display was nice for debugging but shouldnt be shipped so the client is able to clear the pressure
         #self.pressure_display_label.pack(side=tk.RIGHT)
         self.pressure_scale = ttk.Scale(self.pressure_frame, from_=0, to=180, orient=tk.HORIZONTAL, variable=self.pressure_var, command=lambda v: self.on_pressure_change(v, self.pressure_display_label))
 
